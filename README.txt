f06af6346c799e8219f8888d744cccdd7d86f507
Shawn O'Hare <ohare.shawn@gmail.com>
2013-09-25 20:53:42 -0700

    first commit

---
+++ README.txt
@@ -0,0 +1,12 @@
+This program clusters posts in the MLComp 20news-18828 dataset using sklearn's KMeans via 
+an augmentation to the sklearn TfidfVectorizer that utilizes 
+the SnowballStemmer from the NTLK.  
+
+Given a new post in the form a string, get_related(post) will
+predict which cluster post belongs to and then fetch three
+<<<<<<< HEAD
+posts from said cluster with various similarities.  
+
+The mlcomp data needs to be downloaded, and then put in a directory
+data/, or else the MLCOMP_DIR variable in the Python script needs to be changed
+to point to the location where the mlcomp data is downloaded.
---
+++ post_similarity.py
@@ -0,0 +1,72 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import scipy as sp
+import nltk.stem, os
+import sklearn.datasets
+from sklearn.cluster import KMeans
+
+
+def main():
+    vectorizer = StemmedTfidfVectorizer(min_df = 10, max_df=.5, stop_words = 'english', decode_error = 'ignore')
+    
+    # Get the 20 newsgroups dataset and use sklearn's customer loaders for mlcomp data
+<<<<<<< HEAD
+    # MLCOMP_DIR points to the directory containing the mlcomp data.
+=======
+    
+>>>>>>> 2250955f96d32de7ffd5ba55adea7070f7c767c8
+    MLCOMP_DIR = 'data/'
+    full_set =  sklearn.datasets.load_mlcomp("20news-18828", mlcomp_root=MLCOMP_DIR)
+    train_set = sklearn.datasets.load_mlcomp("20news-18828", "train", mlcomp_root=MLCOMP_DIR)
+    test_set =  sklearn.datasets.load_mlcomp("20news-18828", "test", mlcomp_root=MLCOMP_DIR)
+    # vectorize the data using StemmedTfidVectorizer
+    vect_train_data = vectorizer.fit_transform(train_set.data)
+    vect_test_data = vectorizer.transform(test_set.data)
+    vect_full_set = vectorizer.transform(full_set.data)
+    # cluster the data
+    num_clusters = 50
+    km = KMeans(n_clusters = num_clusters, init='random', n_init=1, verbose=1)
+    km.fit(vect_train_data)
+    
+    # testing
+    new_post = "Hey guys.  I think that polar bears are pretty cool animals, but I'm concerned that they won't be around for much longer due to global warming.  What can I do to help stop global warming?"
+    get_related(new_post)
+     
+    
+
+def get_related(post):
+    """Given a string consisting of a post, predicts its cluster using the already trained km object.
+    Returns three related posts from the same cluster.
+    
+    Args: 
+        post (string) - a string consisting of a post to be assigned to a cluster
+    
+    Return: An index array corresponding to three posts in the same cluster as post of varying similarities.    
+    """
+    post_vec = vectorizer.transform([post])
+    post_label = km.predict(post_vec)[0]
+    # get all indicies of posts in the same cluster as the input post
+    cluster_indices = (km.labels_ == post_label).nonzero()[0]
+    related = []
+    for i in cluster_indices:
+        dist = sp.linalg.norm((post_vec - vect_full_set[i]).toarray())
+        related.append((dist,full_set.data[i]))
+    related = sorted(related)
+    post_1 = related[0]
+    post_2 = related[len(related)/2]
+    post_3 = related[-1]
+    posts = [related[0], related[len(related)/2], related[-1]]
+    for item in posts:
+        print("Similarity:",item[0], '\n Post:', item[1])
+    
+
+# Overwrite the build_analyzer method of sklearn.CountVectorizer to include stemming
+class StemmedTfidfVectorizer(TfidfVectorizer):
+    def build_analyzer(self):
+        analyzer = super(StemmedTfidfVectorizer,self).build_analyzer()
+        eng_stem = nltk.stem.SnowballStemmer('english')
+        return lambda doc: (eng_stem.stem(w) for w in analyzer(doc))
+<<<<<<< HEAD
+
+main()
+
+        
