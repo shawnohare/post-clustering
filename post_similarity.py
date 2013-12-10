115dbd555dcbc0da81c0a1d6b7836d6c17161007
Shawn O'Hare <ohare.shawn@gmail.com>
2013-12-10 15:32:56 -0800

    clean up

--- post_similarity.py
+++ post_similarity.py
@@ -6,15 +6,15 @@ from sklearn.cluster import KMeans
 
 
 def main():
-    vectorizer = StemmedTfidfVectorizer(min_df = 10, max_df=.5, stop_words = 'english', decode_error = 'ignore')
+    vectorizer = StemmedTfidfVectorizer(
+            min_df = 10, 
+            max_df=.5, 
+            stop_words = 'english', 
+            decode_error = 'ignore')
     
     # Get the 20 newsgroups dataset and use sklearn's customer loaders for mlcomp data
-<<<<<<< HEAD
-    # MLCOMP_DIR points to the directory containing the mlcomp data.
-=======
     
->>>>>>> 2250955f96d32de7ffd5ba55adea7070f7c767c8
-    MLCOMP_DIR = 'data/'
+    MLCOMP_DIR = 'data/' # points to the data directory
     full_set =  sklearn.datasets.load_mlcomp("20news-18828", mlcomp_root=MLCOMP_DIR)
     train_set = sklearn.datasets.load_mlcomp("20news-18828", "train", mlcomp_root=MLCOMP_DIR)
     test_set =  sklearn.datasets.load_mlcomp("20news-18828", "test", mlcomp_root=MLCOMP_DIR)
@@ -28,11 +28,9 @@ def main():
     km.fit(vect_train_data)
     
     # testing
-    new_post = "Hey guys.  I think that polar bears are pretty cool animals, but I'm concerned that they won't be around for much longer due to global warming.  What can I do to help stop global warming?"
-    get_related(new_post)
+    new_post = "Hey guys.  I think that polar bears are pretty cool animals."
      
     
-
 def get_related(post):
     """Given a string consisting of a post, predicts its cluster using the already trained km object.
     Returns three related posts from the same cluster.
